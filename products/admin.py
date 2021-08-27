b591742d7633202013e19a46b2351ff8516b0f40
andrewboyd79 <andrewboyd79@googlemail.com>
2021-08-27 18:59:30 +0000

    Customised model admin to set display and ordering

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 from .models import Product, Category, Type
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Type)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'image',
+        'name',
+        'brief_description',
+        'starting_price',
+        'category',
+        'type'
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Type, TypeAdmin)
