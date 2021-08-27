17d5b03ff61b918d9deedeaaebd4deccb03d99c5
andrewboyd79 <andrewboyd79@googlemail.com>
2021-08-27 08:09:12 +0000

    Add media and update settings to access media img

--- espressorders/settings.py
+++ espressorders/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # for allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
