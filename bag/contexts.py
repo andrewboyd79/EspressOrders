2090e03a88fd9daff3dae7a022a995d846ed62c2
andrewboyd79 <andrewboyd79@googlemail.com>
2021-09-01 20:42:33 +0000

    Updated shopping basket template

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
@@ -5,6 +7,21 @@ def basket_contents(request):
     basket_items = []
     basket_total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        starting_price = product.starting_price
+        item_total = quantity * product.starting_price
+        basket_total += item_total
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+            'starting_price': starting_price,
+            'item_total': item_total,
+        })
 
     context = {
         'basket_items': basket_items,
