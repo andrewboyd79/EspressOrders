c41b1a1ef2577a9a6294a4009e0e43f38aea5046
andrewboyd79 <andrewboyd79@googlemail.com>
2021-09-16 20:32:01 +0000

    Set user authentication requirement on profile block

--- checkout/views.py
+++ checkout/views.py
@@ -126,7 +126,7 @@ def checkout(request):
                 })
             except UserProfile.DoesNotExist:
                 order_form = OrderForm()
-        else:        
+        else:
             order_form = OrderForm()
 
     if not stripe_public_key:
@@ -150,22 +150,25 @@ def checkout_success(request, order_number):
 
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    profile = UserProfile.objects.get(user=request.user)
 
-    # Attach the user's profile to the order
-    order.user_profile = profile
-    order.save()
+    if request.user.is_authenticated:
 
-    # Save the user's info
-    if save_info:
-        profile_data = {
-            'default_full_name': order.full_name,
-            'default_phone_number': order.phone_number,
-            'default_email': order.email,
-        }
-        user_profile_form = UserProfileForm(profile_data, instance=profile)
-        if user_profile_form.is_valid():
-            user_profile_form.save()
+        profile = UserProfile.objects.get(user=request.user)
+
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_full_name': order.full_name,
+                'default_phone_number': order.phone_number,
+                'default_email': order.email,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
     messages.success(request, f'Your order was successfully processed! \
         Your order will be available for collection shortly.\
