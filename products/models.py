34b7bce874122a2dcce196d3ee1ccbf239ff124a
andrewboyd79 <andrewboyd79@googlemail.com>
2021-09-19 18:53:04 +0000

    Create add product functionality

--- products/models.py
+++ products/models.py
@@ -34,7 +34,7 @@ class Product(models.Model):
     starting_price = models.DecimalField(max_digits=3, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    has_attributes = models.BooleanField (default=False, null=True, blank=True)
+    has_attributes = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
